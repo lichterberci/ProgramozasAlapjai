54d671f85cba4bd9c3414307dd84c661145fd7bd
lichterberci <lichterberci@gmail.com>
2022-09-08 21:29:40 +0200

    0zh hf

--- 0zh_hf/main.c
+++ 0zh_hf/main.c
@@ -1,39 +1,180 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// reading a file
+struct Room {
 
-void ReadFile (const char* fileName) {
-
-    FILE* fp;
-
-    fp = fopen (fileName, "r");
-
-    char buffer[100];
-
-    fgets (buffer, sizeof (buffer), fp);
-
-    printf ("%s", buffer);
-
-    fclose (fp);
+    int roomNumber;
+    char name[50]; // use strcpy !!! 
+    struct Room* nextPtr;
 
 };
 
-void WriteFile (const char* fileName, const char* message) {
+void PrintFloor (struct Room* firstRoom) {
 
-    FILE* fp;
+    struct Room* currentRoom = firstRoom;
 
-    fp = fopen (fileName, "w");
+    while (currentRoom != NULL) {
 
-    fprintf (fp, message);
+        printf ("Szoba %d - %s\n", currentRoom->roomNumber, currentRoom->name);
+        currentRoom = currentRoom->nextPtr;
 
-    fclose (fp);
+    }
+
+};
+
+struct Room* ReadFloorFromFile (const char* fileName) {
+
+    FILE* file = fopen(fileName, "r");
+
+    char line[64];
+    char roomNumberStringBuffer[4];
+
+    struct Room* startPtr = NULL;
+    struct Room* prevRoomPtr = NULL;
+
+    while (fgets(line, sizeof(line), file)) {
+
+        char* ptrToFirstSpace = strchr (line, ' ');
+
+        struct Room* currentRoom = malloc(sizeof(struct Room));
+
+        // get rid of \n\r
+
+        for (size_t i = 0; i < strlen(line); i++)
+        {
+            if (line[i] == '\n' || line[i] == '\r')
+                line[i] = '\0';
+        }               
+
+        // get room number
+
+        for (size_t i = 0; i < ptrToFirstSpace - line; i++)
+        {
+            roomNumberStringBuffer[i] = line[i];
+        }
+        
+        roomNumberStringBuffer[ptrToFirstSpace - line] = 0;
+
+        int roomNumber = atoi (roomNumberStringBuffer);
+        
+        // get name
+
+        char nameBuffer[64];
+
+        for (size_t i = 0; i < strchr (line, '\0') - ptrToFirstSpace; i++) {
+            nameBuffer[i] = line[i + (int)(ptrToFirstSpace - line) + 1];
+        }
+
+        nameBuffer[strchr (line, '\0') - ptrToFirstSpace] = 0;
+
+        // set up current room
+
+        currentRoom->roomNumber = roomNumber;
+        strcpy(currentRoom->name, nameBuffer);
+        currentRoom->nextPtr = NULL;
+
+        // update list
+
+        if (startPtr == NULL)
+            startPtr = currentRoom;
+        else
+            prevRoomPtr->nextPtr = currentRoom;
+
+        prevRoomPtr = currentRoom;
+    }
+
+    fclose(file);
+
+    return startPtr;
+
+};
+
+void GetNumberOfRooms (struct Room* firstRoom, int numRooms[8]) {
+
+    // initialize
+
+    for (size_t i = 0; i < 8; i++) {
+        numRooms[i] = 0;
+    }
+
+    // read through the list and add 1 to the respective floor counter
+
+    struct Room* currentPtr = firstRoom;
+
+    while (currentPtr != NULL) {
+
+        int floor = currentPtr->roomNumber / 100;
+
+        numRooms[floor]++;
+
+        currentPtr = currentPtr->nextPtr;
+    }
+
+}
+
+int GetMostPopulatedFloor (int numRooms[8]) {
+
+    int maxIndex = 0;
+    int maxCount = numRooms[0];
+
+    for (int i = 1; i < 8; i++) {
+
+        if (numRooms[i] > maxCount) {
+            maxIndex = i;
+            maxCount = numRooms[i];
+        }
+    }
+
+    return maxIndex;
+
+};
+
+struct Room* GetRoomByName (struct Room* firstRoom, const char* name) {
+
+    struct Room* currentPtr = firstRoom;
+
+    while (currentPtr != NULL) {
+
+        if (strcmp (currentPtr->name, name) == 0) {
+            return currentPtr;
+        }
+
+        currentPtr = currentPtr->nextPtr;
+    }
+
+    return NULL;
+
+};
+
+void ComplicatedFunction (const char* fileName, const char* name) {
+
+    struct Room* list = ReadFloorFromFile (fileName);
+
+    struct Room* roomByName = GetRoomByName (list, name);
+
+    if (roomByName == NULL) {
+        printf ("Nem szallt meg!\n");
+        return;
+    }
+
+    int numRooms[8];
+
+    GetNumberOfRooms (list, numRooms);
+
+    int mostPopulatedFloor = GetMostPopulatedFloor(numRooms);
+
+    if (roomByName->roomNumber / 100 == mostPopulatedFloor) {
+        printf("Megszallt es a legzsufoltabb szinten!\n");
+    } else {
+        printf ("Megszallt, de nem a legzsufoltabb szinten!\n");
+    }
 
 };
 
 int main (void) {
 
-    WriteFile ("test.txt", "This is not that bad...");
-    ReadFile ("test.txt");
+    ComplicatedFunction ("test.txt", "Al Bert");
 
     return 0;
 
