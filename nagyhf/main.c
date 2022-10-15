da7e339e3286ec633f524b9553c9c7ecc67aa3f6
lichterberci <lichterberci@gmail.com>
2022-10-15 09:07:16 +0200

    started debug

--- nagyhf/main.c
+++ nagyhf/main.c
@@ -18,16 +18,18 @@ void PrintImagesInfinitely (Dataset dataset) {
 
 int main () {
 
+    printf ("start\n");
+
     const char* trainImagePath = "./data/train-images.idx3-ubyte";
     const char* trainLabelPath = "./data/train-labels.idx1-ubyte";
     const char* testImagePath = "./data/t10k-images.idx3-ubyte";
     const char* testLabelPath = "./data/t10k-labels.idx1-ubyte";
 
-    Dataset trainSet = ReadDatasetFromFile(trainImagePath, trainLabelPath);
-    Dataset testSet = ReadDatasetFromFile(testImagePath, testLabelPath);
+    // Dataset trainSet = ReadDatasetFromFile(trainImagePath, trainLabelPath);
+    // Dataset testSet = ReadDatasetFromFile(testImagePath, testLabelPath);
 
-    if (trainSet.numData == 0 || testSet.numData == 0)
-        exit(-1);
+    // if (trainSet.numData == 0 || testSet.numData == 0)
+    //     exit(-1);
 
     //                        V--- Number of hidden layers, don't forget to update!!!
     Model model = CreateModel(1, 1, SIGMOID, SOFTMAX);
@@ -36,22 +38,22 @@ int main () {
 
     PrintModel(model);
 
-    // LabeledImage dummyImage;
-    // dummyImage.data[0] = 0.3;
-    // dummyImage.label = 0;
+    LabeledImage dummyImage;
+    dummyImage.data[0] = 0.3;
+    dummyImage.label = 0;
 
-    // FitModelForImage(model, &dummyImage, 1.0);
+    FitModelForImage(model, &dummyImage, 1.0);
 
-    // PrintModel(model);
+    PrintModel(model);
 
-    for (int i = 0; i < 100; i++)
-    {
-        FitModelForImage(model, &(trainSet.images[0]), 0.001);
-        if (i % 10 == 0) {
-            Result result = Predict(model, (double*)&(trainSet.images[0].data), NULL);
-            //PrintResult(result);
-        }
-    }
+    // for (int i = 0; i < 100; i++)
+    // {
+    //     FitModelForImage(model, &(trainSet.images[0]), 0.001);
+    //     if (i % 10 == 0) {
+    //         Result result = Predict(model, (double*)&(trainSet.images[0].data), NULL);
+    //         //PrintResult(result);
+    //     }
+    // }
     
 
     printf("Code exited safely!");
