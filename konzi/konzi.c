81c6b8d63fc45fd46c7017837644088a4e3d569e
lichterberci <lichterberci@gmail.com>
2022-10-18 13:46:55 +0200

    konzi cf

--- konzi/konzi.c
+++ konzi/konzi.c
@@ -1,9 +1,32 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "stdbool.h"
 
 int main (void) {
 
-    printf("Hello, World!");
+    char stack[256];
+    int stackptr = 0;
+
+    while (!foef(stdin)) {
+        char c = getchar();
+
+        // csak ({[]}) érdekel
+
+        char * zarojelek = "({[]})";
+
+        if (strchr(zarojelek, c) != -1) {
+            if (strchr(zarojelek, c) < 3) {
+                stack[stackptr++] = c;
+            } else if (strchr(zarojelek, stack[stackptr]) == 6 - strchr(zarojelek, c)) {
+                stackptr--;
+            } else {
+                printf("Error at char '%c'\n", c);
+                return -1;
+            }
+        }
+
+        return 0;
+    }
 
     return 0;
 }
