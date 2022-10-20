577a7ba05498dc6844166a8d10153180ba7f4bab
lichterberci <lichterberci@gmail.com>
2022-10-20 22:48:19 +0200

    minor changes

--- nagyhf/main.c
+++ nagyhf/main.c
@@ -273,11 +273,11 @@ int main () {
         exit(-1);
 
     //                        V--- Number of hidden layers, don't forget to update!!!
-    Model model = CreateModel(3, 800, RELU, 800, RELU, 200, RELU, SOFTMAX);
+    Model model = CreateModel(2, 800, RELU, 800, RELU, SOFTMAX);
     InitModelToRandom(&model, 1.0);
     
-    const int numEpochs = 1;
-    const double learningRate = 1e-9; // should be lower if the model is trained for many epochs
+    const int numEpochs = 5;
+    const double learningRate = 1 * pow(10, -7); // should be lower if the model is trained for many epochs
 
     FitModel(model, trainSet, testSet, numEpochs, learningRate);
     
